2446cceb2e7c1c655b3ec032ddab524fa43ea485
Ahmad Murtaza <ahmad@oxibit.com>
2022-10-07 18:10:41 +0500

    course info screens

--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -1,12 +1,61 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { GdbComponent } from './gdb/gdb.component';
+import { QuizesComponent } from './quizes/quizes.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: "",
+    children: [
+      {
+        path: "",
+        component: CoursesComponent,
+      },
+      {
+        path: ":id",
+        children: [
+          {
+            path: "",
+            component: AboutComponent
+          }, 
+          {
+            path: "announcements",
+            loadChildren: () => import("./announcements/announcements.module").then(m => m.AnnouncementsModule)
+          }, 
+          {
+            path: "assignments",
+            loadChildren: () => import("./assignments/assignments.module").then(m => m.AssignmentsModule)
+          }, 
+          {
+            path: "gdb",
+            loadChildren: () => import("./gdb/gdb.module").then(m => m.GdbModule)
+          }, 
+          {
+            path: "quizes",
+            loadChildren: () => import("./quizes/quizes.module").then(m => m.QuizesModule)
+          }, 
+        ]
+      }
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AssignmentsComponent,
+    GdbComponent,
+    QuizesComponent,
+    AnnouncementsComponent,
+    AboutComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class CoursesModule { }
