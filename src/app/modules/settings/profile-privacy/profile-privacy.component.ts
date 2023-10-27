dfb3054913c844cffeece5f8468820a2aa7c7e94
Sadaf Shakeel <sadafshakeel499@gmail.com>
2023-10-27 16:30:39 +0500

    Updated

--- src/app/modules/settings/profile-privacy/profile-privacy.component.ts
+++ src/app/modules/settings/profile-privacy/profile-privacy.component.ts
@@ -1,15 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ApiService } from 'src/app/services/users/api.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-profile-privacy',
   templateUrl: './profile-privacy.component.html',
-  styleUrls: ['./profile-privacy.component.css']
+  styleUrls: ['./profile-privacy.component.css'],
 })
-export class ProfilePrivacyComponent implements OnInit {
+export class ProfilePrivacyComponent {
+  userDetails: any = {
+    userId: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    managerId: '',
+    departmentId: '',
+    designationId: '',
+    address: '',
+    city: '',
+    state: '',
+    zipcode: '',
+    country: '',
+    imageUrl: '',
+    jobTitle: '',
+    phoneNumber: '',
+    skype: '',
+  };
 
-  constructor() { }
+  constructor(private toastr: ToastrService, private apiServices: ApiService) {}
+
+  @ViewChild('closeModal') closeModal: ElementRef | undefined;
 
   ngOnInit(): void {
+    this.getUserDetails();
   }
 
+  getUserDetails() {
+    const data = {
+      path: 'users/detail',
+      payload: {},
+    };
+    this.apiServices.postRequest(data).subscribe((res) => {
+      const data = res.data;
+      this.userDetails = {
+        userId: data.id,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        managerId: data.managerId,
+        departmentId: data.departmentId,
+        designationId: data.designationId,
+        address: data.userProfile.address,
+        city: data.userProfile.city,
+        state: data.userProfile.state,
+        zipcode: data.userProfile.zipcode,
+        country: data.userProfile.country,
+        imageUrl: data.userProfile.imageUrl,
+        jobTitle: data.userProfile.jobTitle,
+        phoneNumber: data.userProfile.phoneNumber,
+        skype: data.userProfile.skype,
+      };
+      console.log(this.userDetails);
+    });
+  }
+
+  updateUserProfile() {
+    const data = {
+      path: 'users/update/profile',
+      payload: {
+        firstName: this.userDetails.firstName,
+        lastName: this.userDetails.lname,
+        email: this.userDetails.email,
+        jobTitle: this.userDetails.jobTitle,
+        phoneNumber: this.userDetails.phone,
+        skype: this.userDetails.skype,
+        address: this.userDetails.address,
+        city: this.userDetails.city,
+        state: this.userDetails.state,
+        zipcode: this.userDetails.zipcode,
+        country: this.userDetails.country,
+      },
+    };
+    this.apiServices.postRequest(data).subscribe((data) => {
+      if (this.closeModal) {
+        this.closeModal.nativeElement.click();
+      }
+      // user.reset();
+      this.toastr.success('Profile updated successfully!');
+      this.getUserDetails();
+    });
+  }
 }
