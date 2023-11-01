44a7e9c7aeecd66ac6b62c1618fbeb51882fd305
Sadaf Shakeel <sadafshakeel499@gmail.com>
2023-11-01 12:47:49 +0500

    updates

--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -30,7 +30,7 @@ const routes: Routes = [
             component: DetailComponent,
           },
           {
-            path: 'lectures/:id',
+            path: 'lectures/:taskId',
             component: LectureComponent,
           },
         ],
--- src/app/modules/enrollments/enrollments.component.ts
+++ src/app/modules/enrollments/enrollments.component.ts
@@ -1,31 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ApiService } from 'src/app/services/users/api.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-enrollments',
   templateUrl: './enrollments.component.html',
-  styleUrls: ['./enrollments.component.css']
+  styleUrls: ['./enrollments.component.css'],
 })
-export class EnrollmentsComponent implements OnInit {
-
+export class EnrollmentsComponent {
   enrollments: any;
 
-  constructor() { }
+  constructor(private toastr: ToastrService, private apiServices: ApiService) {}
+  @ViewChild('closeModal') closeModal: ElementRef | undefined;
 
   ngOnInit(): void {
-    this.enrollments = [
-      {
-        title: 'O Levels',
-        content: 'Summers 2022',
-        status: 'active',
-        courses: ['Biology', 'Mathematics', 'Physics', 'Pakistan Studies']
-      },
-      {
-        title: 'Matriculation',
-        content: 'Regular Session',
-        status: 'inactive',
-        courses: ['Computer Science', 'Chemistry']
-      },
-    ]
+    this.getEnrollments();
   }
 
+  getEnrollments() {
+    const data = {
+      path: 'enrollments/list',
+      payload: {},
+    };
+    this.apiServices.postRequest(data).subscribe((response) => {
+      this.enrollments = response;
+      console.log(this.enrollments);
+    });
+  }
 }
