b1a03207187f311b70025ccbb5b9a716417a9de1
Ahmad Murtaza <ahmad@oxibit.com>
2023-11-01 14:17:46 +0500

    some updates

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as $ from 'jquery';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { ConfigService } from 'src/app/config/config.service';
 
 @Component({
   selector: 'app-header',
@@ -10,16 +11,24 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class HeaderComponent implements OnInit {
   loggedInUser: any;
+  client: any;
+  imageBaseUrl: string = this.config.ImgBaseURL;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private config: ConfigService
+  ) {}
 
   ngOnInit(): void {
     this.loggedInUser = JSON.parse(this.authService.getUser());
+    this.client = this.loggedInUser.client;
+    console.log(this.client);
   }
 
   logout() {
     this.authService.logOut();
-    this.router.navigate(['/']);
+    this.router.navigate(['/signin']);
   }
 
   closeSidebar() {
