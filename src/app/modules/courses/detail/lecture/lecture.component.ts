d7e772f41fff525172bee3421b28be0f524aaf96
Sadaf Shakeel <sadafshakeel499@gmail.com>
2023-11-07 13:18:18 +0500

    Updated

--- src/app/modules/courses/detail/lecture/lecture.component.ts
+++ src/app/modules/courses/detail/lecture/lecture.component.ts
@@ -5,12 +5,17 @@ import { ToastrService } from 'ngx-toastr';
 import { ApiService } from 'src/app/services/users/api.service';
 import { ConfigService } from 'src/app/config/config.service';
 
+declare var YT: any;
+
 @Component({
   selector: 'app-lecture',
   templateUrl: './lecture.component.html',
   styleUrls: ['./lecture.component.css'],
 })
 export class LectureComponent {
+  private player: any;
+  private interval: any;
+
   courseId: any;
   taskId: any;
 
@@ -52,6 +57,17 @@ export class LectureComponent {
   questionFormType: string = '';
   assessmentQuestionType: string = '';
 
+  passQuiz: boolean = false;
+  showError = false;
+
+  selectedOption: string = '';
+
+  options = [
+    { label: '3', value: '3' },
+    { label: '4', value: '4' },
+    { label: '5', value: '5' },
+  ];
+
   constructor(
     private toastr: ToastrService,
     private apiServices: ApiService,
@@ -60,6 +76,10 @@ export class LectureComponent {
   ) {}
 
   @ViewChild('closeModal') closeModal: ElementRef | undefined;
+  @ViewChild('questionModalBtn') questionModalBtn: ElementRef | undefined;
+  @ViewChild('questionModalBtnClose') questionModalBtnClose:
+    | ElementRef
+    | undefined;
 
   ngOnInit(): void {
     this.courseId = this.route.snapshot.paramMap.get('id');
@@ -71,6 +91,8 @@ export class LectureComponent {
     this.getTaskDetails();
 
     this.getAssessments();
+
+    this.loadVideo();
 
     this.instructor = {
       name: 'Dr. Saleha Naghmi',
@@ -292,5 +314,102 @@ export class LectureComponent {
       answer: '',
       type: '',
     };
+  }
+
+  // checkQuizTime() {
+  //   var quizTime = 2;
+
+  //   this.videoPlayer?.nativeElement.addEventListener('timeupdate', () => {
+  //     if (
+  //       this.videoPlayer.nativeElement.currentTime >= quizTime &&
+  //       !this.videoPlayer.nativeElement.paused &&
+  //       !this.showQuiz &&
+  //       !this.passQuiz &&
+  //       !this.showError
+  //     ) {
+  //       this.videoPlayer.nativeElement.pause();
+  //       this.showQuiz = true;
+  //     }
+  //   });
+  // }
+
+  // checkAnswer() {
+  //   if (this.selectedOption === '4') {
+  //     this.videoPlayer.nativeElement.play();
+  //     this.showQuiz = false;
+  //     this.passQuiz = true;
+  //     this.showError = false;
+  //   } else {
+  //     this.videoPlayer.nativeElement.currentTime;
+  //     // this.videoPlayer.nativeElement.play();
+  //     this.showQuiz = false;
+  //     this.passQuiz = false;
+  //     this.showError = true;
+  //   }
+  //   this.selectedOption = '';
+  // }
+
+  loadVideo() {
+    const videoId = 'sQD7kaZ5h0s';
+
+    this.player = new YT.Player('youtube-player', {
+      videoId: videoId,
+      playerVars: {
+        controls: 1,
+        autoplay: 0,
+        enablejsapi: 1,
+        iv_load_policy: 3,
+      },
+      events: {
+        onReady: (event: any) => {
+          this.player = event.target;
+          this.initModalInterval();
+        },
+      },
+    });
+  }
+
+  initModalInterval() {
+    this.interval = setInterval(() => {
+      const currentTime = this.player.getCurrentTime();
+      console.log(currentTime);
+
+      if (currentTime >= 3) {
+        this.pauseVideoAndShowModal();
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  pauseVideoAndShowModal() {
+    this.player.pauseVideo();
+    console.log('asdas');
+
+    if (this.questionModalBtn) this.questionModalBtn.nativeElement.click();
+  }
+
+  checkAnswer() {
+    if (this.selectedOption === '4') {
+      if (this.questionModalBtnClose)
+        this.questionModalBtnClose?.nativeElement.click();
+      this.player.playVideo();
+
+      // this.videoPlayer.nativeElement.play();
+      // this.showQuiz = false;
+      // this.passQuiz = true;
+      // this.showError = false;
+    } else {
+      // this.videoPlayer.nativeElement.currentTime;
+      // // this.videoPlayer.nativeElement.play();
+      // this.showQuiz = false;
+      // this.passQuiz = false;
+      // this.showError = true;
+    }
+    this.selectedOption = '';
+  }
+
+  retryQuiz() {
+    this.showError = false;
+    this.passQuiz = true;
   }
 }
