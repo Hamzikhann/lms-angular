258083f6e6948f3b428b4f7f2711c0b8a18f944f
Ahmad Murtaza <ahmad@oxibit.com>
2023-11-08 14:25:53 +0500

    ui updates

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ApiService } from 'src/app/services/users/api.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  courses: any;
+  loggedInUser: any;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private apiServices: ApiService
+  ) {}
 
   ngOnInit(): void {
+    this.loggedInUser = JSON.parse(this.authService.getUser());
+    if (this.loggedInUser.role.title == 'Client') {
+      this.getAssignedCourses();
+    } else if (this.loggedInUser.role.title == 'User') {
+      this.getCourses();
+    }
   }
 
+  getCourses() {
+    const data = {
+      path: 'courses/list',
+      payload: {},
+    };
+    this.apiServices.postRequest(data).subscribe((data) => {
+      this.courses = data;
+    });
+  }
+
+  getAssignedCourses() {
+    const data = {
+      path: 'courses/list',
+      payload: {},
+    };
+    this.apiServices.postRequest(data).subscribe((data) => {
+      this.courses = data;
+    });
+  }
 }
