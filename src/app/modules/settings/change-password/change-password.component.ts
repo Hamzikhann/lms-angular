8893184218c3311d1ba6c87d5776b64b2bf75e91
Ashar Nadeem <asharnadeem002@gmail.com>
2024-03-26 14:27:02 +0500

    "Mark as complete" reversed, change-password edited

--- src/app/modules/settings/change-password/change-password.component.ts
+++ src/app/modules/settings/change-password/change-password.component.ts
@@ -1,112 +1,119 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ApiService } from 'src/app/services/users/api.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.css'], // Assuming you have a CSS file
+  styleUrls: ['./change-password.component.css'],
 })
 export class ChangePasswordComponent implements OnInit {
-  changePasswordForm: FormGroup<any> = new FormGroup({});
+  @ViewChild('oldPasswordInput') oldPasswordInput: ElementRef | undefined;
+  newPassword = '';
+  oldPassword = '';
+  constructor(private toastr: ToastrService, private apiServices: ApiService) {}
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private toastr: ToastrService,
-    private apiServices: ApiService
-  ) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    this.changePasswordForm = this.formBuilder.group(
-      {
-        oldPassword: ['', Validators.required],
-        newPassword: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(8),
-            Validators.maxLength(16),
-          ],
-        ],
-        passwordConfirmation: ['', Validators.required],
-      },
-      {
-        validator: this.passwordMatchValidator,
-      }
-    );
-  }
-
-  get f() {
-    return this.changePasswordForm.controls;
-  }
-
-  passwordMatchValidator(control: FormGroup): { [key: string]: any } | null {
-    const password = control.get('newPassword');
-    const confirmPassword = control.get('passwordConfirmation');
-    return password &&
-      confirmPassword &&
-      password.value !== confirmPassword.value
-      ? { passwordMismatch: true }
-      : null;
-  }
-
-  changePassword() {
-    if (this.changePasswordForm.invalid) {
-      return;
-    }
-
+  changePassword(form: any) {
     const data = {
       path: 'users/update/password',
       payload: {
-        oldPassword: this.changePasswordForm.value.oldPassword, // Access form values here
-        password: this.changePasswordForm.value.newPassword,
-        passwordConfirmation:
-          this.changePasswordForm.value.passwordConfirmation,
+        oldPassword: form.value.oldPassword,
+        password: form.value.newPassword,
+        passwordConfirmation: form.value.confirmPassword,
       },
     };
-
-    this.apiServices.postRequest(data).subscribe(
-      (response) => {
-        // Handle successful password change (e.g., reset, display success message)
-        this.changePasswordForm.reset();
-        this.toastr.success('Password changed successfully!');
-      },
-      (error) => {
-        // Handle errors from the API request
-        console.error(error);
-        this.toastr.error('Error changing password', error.message); // Display error
-      }
-    );
+    this.oldPassword = form.value.oldPassword;
+    if (this.newPassword === this.oldPassword) {
+      this.toastr.error('New password cannot be same as the old password');
+      return;
+    }
+    this.apiServices.postRequest(data).subscribe((data) => {
+      form.reset();
+      this.toastr.success('Password changed successfully!');
+    });
   }
 }
 
 // import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 // import { ApiService } from 'src/app/services/users/api.service';
 // import { ToastrService } from 'ngx-toastr';
 
 // @Component({
 //   selector: 'app-change-password',
 //   templateUrl: './change-password.component.html',
-//   styleUrls: ['./change-password.component.css'],
+//   styleUrls: ['./change-password.component.css'], // Assuming you have a CSS file
 // })
 // export class ChangePasswordComponent implements OnInit {
-//   constructor(private toastr: ToastrService, private apiServices: ApiService) {}
+//   changePasswordForm: FormGroup<any> = new FormGroup({});
 
-//   ngOnInit(): void {}
+//   constructor(
+//     private formBuilder: FormBuilder,
+//     private toastr: ToastrService,
+//     private apiServices: ApiService
+//   ) {}
 
-//   changePassword(form: any) {
+//   ngOnInit(): void {
+//     this.changePasswordForm = this.formBuilder.group(
+//       {
+//         oldPassword: ['', Validators.required],
+//         newPassword: [
+//           '',
+//           [
+//             Validators.required,
+//             Validators.minLength(8),
+//             Validators.maxLength(16),
+//           ],
+//         ],
+//         passwordConfirmation: ['', Validators.required],
+//       },
+//       {
+//         validator: this.passwordMatchValidator,
+//       }
+//     );
+//   }
+
+//   get f() {
+//     return this.changePasswordForm.controls;
+//   }
+
+//   passwordMatchValidator(control: FormGroup): { [key: string]: any } | null {
+//     const password = control.get('newPassword');
+//     const confirmPassword = control.get('passwordConfirmation');
+//     return password &&
+//       confirmPassword &&
+//       password.value !== confirmPassword.value
+//       ? { passwordMismatch: true }
+//       : null;
+//   }
+
+//   changePassword() {
+//     if (this.changePasswordForm.invalid) {
+//       this.toastr.error('Error changing password');
+//       return;
+//     }
+
 //     const data = {
 //       path: 'users/update/password',
 //       payload: {
-//         oldPassword: form.value.oldPassword,
-//         password: form.value.newPassword,
-//         passwordConfirmation: form.value.confirmPassword,
+//         oldPassword: this.changePasswordForm.value.oldPassword,
+//         password: this.changePasswordForm.value.newPassword,
+//         passwordConfirmation:
+//           this.changePasswordForm.value.passwordConfirmation,
 //       },
 //     };
-//     this.apiServices.postRequest(data).subscribe((data) => {
-//       form.reset();
-//       this.toastr.success('Password changed successfully!');
-//     });
+
+//     this.apiServices.postRequest(data).subscribe(
+//       (response) => {
+//         this.changePasswordForm.reset();
+//         this.toastr.success('Password changed successfully!');
+//       },
+//       (error) => {
+//         console.error(error);
+//         this.toastr.error('Error changing password', error.message);
+//       }
+//     );
 //   }
 // }
